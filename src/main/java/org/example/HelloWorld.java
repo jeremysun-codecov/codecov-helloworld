239c79741595fcf251abff1779c620ad12749ae5
Jeremy Sun <yunxuan_sun@apple.com>
2020-05-25 16:06:06 -0700

    first commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class HelloWorld {
+
+	public String fizzBuzz(int num) {
+		if (num % 3 == 0 && num % 5 == 0) {
+			return "FizzBuzz";
+		} else if (num % 3 == 0) {
+			return "Fizz";
+		} else if (num % 5 == 0) {
+			return "Buzz";
+		} else {
+			return "";
+		}
+	}
+
+}
